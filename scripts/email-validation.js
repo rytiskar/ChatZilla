a8d9c18d52de5f302749567a1c0b2dea6814a903
Povilas Pliuskus <PovilasPliuskus@gmail.com>
2023-09-05 10:12:11 +0300

    Move JavaScript files to a new directory scripts
    Add a new file in the scripts directory called email-validation.js.
    
    Move the email validation functions to the newly created file. Add a type=module attribute to the script tag.
    
    Add event listener to the #create-account-button div.

---
+++ scripts/email-validation.js
@@ -0,0 +1,19 @@
+const validateEmailRegex = (email) => {
+    return email.match(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
+export const validateEmail = (labelID, inputID) => {
+    const $label = $(labelID);
+    const email = $(inputID).val();
+   
+     if (validateEmailRegex(email)) {
+        $label.css('color', 'green');
+    }
+    else {
+        $label.css('color', 'red');
+    }
+
+    return false;
+};
--- scripts/sign-up-script.js
+++ scripts/sign-up-script.js
@@ -1,3 +1,8 @@
+import { validateEmail } from "./email-validation.js";
+
+document.getElementById('create-account-button').addEventListener('click', createAccountClick);
+
+
 function createAccountClick() {
     const email = getInputValue('input-email');
     const username = getInputValue('input-username');
@@ -17,25 +22,8 @@ function getInputValue(elementsID) {
     return elementsValue;
 }
 
-const validateEmail = (email) => {
-    return email.match(
-      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
-};
-
-const validate = () => {
-    const $label = $('#input-email-label');
-    const email = $('#input-email').val();
-   
-     if (validateEmail(email)) {
-        $label.css('color', 'green');
-    }
-    else {
-        $label.css('color', 'red');
-    }
-
-    return false;
-};
 
 // Constantly checks for user's input validation state
-$('#input-email').on('input', validate);
+$('#input-email').on('input', function() {
+    validateEmail('#input-email-label', '#input-email'); }
+);
